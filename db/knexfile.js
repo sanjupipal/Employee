64b1b615dfdb1d329462efcab8529adb52faa957
_Sanju <sanju.pipal@dhwaniris.com>
2022-01-27 20:17:41 +0530

    connection change

--- db/knexfile.js
+++ db/knexfile.js
@@ -2,9 +2,12 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: {
+      host: process.env.POSTGRES_HOST,
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT,
+      ssl: { rejectUnauthorized: false },
     },
     seeds: {
       tableName: "knex_seeds",
